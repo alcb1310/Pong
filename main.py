a6ae72526efdceb86fe8ef705deb17e5de6406c9
Andrés Court <andres@andrescourt.com>
2022-09-15 18:02:24 -0500

    Make the ball move and bounce off the top and bottom walls as well as the paddles and if the ball gets out of bounce it restarts from the center of the screen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import time
 from turtle import Screen
 from pongscreen import PongScreen
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.setup(width=900, height=600)
@@ -21,8 +23,29 @@ player_2.create_paddle("right")
 screen.onkey(player_2.turn_up, "Up")
 screen.onkey(player_2.turn_down, "Down")
 
+ball = Ball()
+
 is_playing = True
 while is_playing:
     screen.update()
+    ball.move_ball()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_wall()
+
+    if ball.xcor() >= 370 and ball.distance(player_2) < 40:
+        ball.bounce_paddle()
+    elif ball.xcor() >= 370:
+        ball.restart()
+        screen.update()
+        time.sleep(0.5)
+
+    if ball.xcor() <= -370 and ball.distance(player_1) < 40:
+        ball.bounce_paddle()
+    elif ball.xcor() <= -370:
+        ball.restart()
+        screen.update()
+        time.sleep(0.5)
+
 
 screen.exitonclick()
